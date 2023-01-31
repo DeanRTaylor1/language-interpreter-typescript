8e0352cbe1e8bdd22d14d0b9e921951a3d7476e5
deanrtaylor1 <deanrtaylor@hotmail.com>
2023-01-31 20:59:50 +0700

    added tree print and Visitor generic interfaces

--- tools/expr/Expr.ts
+++ tools/expr/Expr.ts
@@ -1,37 +1,54 @@
-import {Token} from '../../src/token-type'
+import { Token } from '../../src/token-type'
+ 
+export interface Expr { 
+ accept<R>(visitor: Visitor<R>): R
+} 
 
-
-export interface Expr {
-
-}
-export class Binary implements Expr {
-  readonly left: Expr;
-  readonly operator: Token;
-  readonly right: Expr;
-  constructor(left: Expr, operator: Token, right: Expr) {
-    this.left = left;
-    this.operator = operator;
-    this.right = right;
+export interface Visitor<R> {
+   visitBinaryExpr(expr: Binary): R
+   visitGroupingExpr(expr: Grouping): R
+   visitLiteralExpr(expr: Literal): R
+   visitUnaryExpr(expr: Unary): R
+ }
+   export class Binary implements Expr {
+    readonly left:Expr;
+    readonly operator:Token;
+    readonly right:Expr;
+     constructor(left:Expr, operator:Token, right:Expr) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right; 
+ }
+ accept<R>(visitor: Visitor<R>){
+    return visitor.visitBinaryExpr(this)
   }
 }
-export class Grouping implements Expr {
-  readonly expression: Expr;
-  constructor(expression: Expr) {
-    this.expression = expression;
+   export class Grouping implements Expr {
+    readonly expression:Expr;
+     constructor(expression:Expr) {
+      this.expression = expression; 
+ }
+ accept<R>(visitor: Visitor<R>){
+    return visitor.visitGroupingExpr(this)
   }
 }
-export class Literal implements Expr {
-  readonly value: any;
-  constructor(value: any) {
-    this.value = value;
+   export class Literal implements Expr {
+    readonly value:any;
+     constructor(value:any) {
+      this.value = value; 
+ }
+ accept<R>(visitor: Visitor<R>){
+    return visitor.visitLiteralExpr(this)
   }
 }
-export class Unary implements Expr {
-  readonly operator: Token;
-  readonly right: Expr;
-  constructor(operator: Token, right: Expr) {
-    this.operator = operator;
-    this.right = right;
+   export class Unary implements Expr {
+    readonly operator:Token;
+    readonly right:Expr;
+     constructor(operator:Token, right:Expr) {
+      this.operator = operator;
+      this.right = right; 
+ }
+ accept<R>(visitor: Visitor<R>){
+    return visitor.visitUnaryExpr(this)
   }
 }
-
--- tools/index.ts
+++ tools/index.ts
@@ -19,13 +19,20 @@ const start = () => {
 }
 
 const defineAst = (outDir: string, baseName: string, types: Array<string>): void => {
-  const path: string = `${__dirname}/${outDir}/${baseName}.ts`
+  const path: string = `${__dirname}/../src/${baseName}.ts`
 
   const writer = fs.createWriteStream(path, {
     flags: 'a'
-   })
+  })
 
-  writer.write(`export interface ${baseName} { \r\n \r\n }`)
+  writer.write(`import { Token } from '../../src/token-type'\r\n \r\n`)
+
+  writer.write(`export interface ${baseName} { \r\n `)
+
+  writer.write(`accept<R>(visitor: Visitor<R>): R`)
+  writer.write("\r\n} \r\n")
+
+  defineVisitor(writer, baseName, types)
 
   for (const type of types) {
     const className: string = type.split(":")[0].trim();
@@ -38,27 +45,42 @@ const defineAst = (outDir: string, baseName: string, types: Array<string>): void
 
 const defineType = (writer: fs.WriteStream, baseName: string, className: string, fieldList: string) => {
 
-  
+
   const fields: string[] = fieldList.split(", ");
 
   writer.write("\r\n   export class " + className + " implements " + baseName + " {")
 
 
-  for(const field of fields){
-       writer.write("\r\n    readonly " + field.replace(/-\s+/g, ':') + ";")
+  for (const field of fields) {
+    writer.write("\r\n    readonly " + field.replace(/-\s+/g, ':') + ";")
   }
 
   writer.write("\r\n     " + "constructor(" + fieldList.replace(/-\s+/g, ':') + ") {")
 
-  for(const field of fields){
+  for (const field of fields) {
     let name: string = field.split(" ")[0];
     console.log(name.slice(0, -1))
     writer.write("\r\n      this." + name.slice(0, -1) + " = " + name.slice(0, -1) + ";")
   }
 
-  writer.write("\r\n} \r\n }")
+  writer.write(" \r\n }")
+  writer.write(`\r\n accept<R>(visitor: Visitor<R>){
+    return visitor.visit${className}${baseName}(this)
+  }`)
+  writer.write("\r\n}")
 
 
+
+}
+
+const defineVisitor = (writer: fs.WriteStream, baseName: string, types: string[]) => {
+  writer.write(`\r\nexport interface Visitor<R> {`)
+
+  for (const type of types) {
+    const typeName: string = type.split(":")[0].trim();
+    writer.write(`\r\n   visit${typeName}${baseName}(${baseName.toLowerCase()}: ${typeName}): R`)
+  }
+  writer.write(`\r\n }`)
 
 }
 
